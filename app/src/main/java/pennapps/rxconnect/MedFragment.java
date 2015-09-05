3c650d80b840e65def9694d91cb40ddb0c0d32a7
Devansh Kukreja <devanshkukreja1@gmail.com>
2015-09-05 02:41:59 -0400

    smexy new refresh view

--- app/src/main/java/pennapps/rxconnect/MedFragment.java
+++ app/src/main/java/pennapps/rxconnect/MedFragment.java
@@ -1,15 +1,21 @@
 package pennapps.rxconnect;
 
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.tuesda.walker.circlerefresh.CircleRefreshLayout;
+
 /**
  * Created by devanshk on 9/5/15.
  */
 public class MedFragment extends Fragment {
+    static CircleRefreshLayout mRefreshLayout;
 
     public MedFragment() {
     }
@@ -19,6 +25,45 @@ public class MedFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_meds, container, false);
 
+        mRefreshLayout = (CircleRefreshLayout)v.findViewById(R.id.refresh_layout);
+        mRefreshLayout.setOnRefreshListener(new CircleRefreshLayout.OnCircleRefreshListener() {
+            @Override
+            public void completeRefresh() {
+
+            }
+
+            @Override
+            public void refreshing() {
+
+            }
+        });
+
         return v;
+    }
+
+    void populateData(){
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("https://www.google.com", new AsyncHttpResponseHandler() {
+
+            @Override
+            public void onStart() {
+                // called before request is started
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                // called when response HTTP status is "200 OK"
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+            }
+
+            @Override
+            public void onRetry(int retryNo) {
+                // called when request is retried
+            }
+        });
     }
 }
