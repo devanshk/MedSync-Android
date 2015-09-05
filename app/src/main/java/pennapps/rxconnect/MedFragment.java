9630e0a1051d5c3e582b062c693d7cd73118091f
Devansh Kukreja <devanshkukreja1@gmail.com>
2015-09-05 01:33:27 -0400

    Added Expandable View Class

--- app/src/main/java/pennapps/rxconnect/MedFragment.java
+++ app/src/main/java/pennapps/rxconnect/MedFragment.java
@@ -17,7 +17,7 @@ public class MedFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_main, container, false);
+        View v = inflater.inflate(R.layout.fragment_meds, container, false);
 
         return v;
     }
